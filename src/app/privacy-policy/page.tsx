b46ee63bd33dbb767558ba493116b22e086ddace
Mohamed-Hashim-km <hashimhx3@gmail.com>
2025-04-26 18:00:46 +0530

    second

---
+++ src/app/privacy-policy/page.tsx
@@ -0,0 +1,12 @@
+import PrivacySection from '@/components/PrivacyPolicyPageComponents/PrivacySection/PrivacySection'
+import React from 'react'
+
+const page = () => {
+  return (
+    <section>
+        <PrivacySection/>
+    </section>
+  )
+}
+
+export default page
---
+++ src/components/PrivacyPolicyPageComponents/PrivacySection/PrivacySection.tsx
@@ -0,0 +1,45 @@
+"use client";
+import React, { useState } from "react";
+import grievanceRedressalCell from "../../../utils/privacyData/privacyData.json";
+
+const PrivacySection = () => {
+  const [selectedIndex, setSelectedIndex] = useState<number>(0);
+
+  return (
+    <section className="py-10 xl:py-36 text-black lg2:px-24 mx-5 overflow-hidden">
+      <div className="">
+        <h1 className="text-[30px] lg:text-[54px] font-bold pb-1 lg:pb-10 text-black">Privacy Policy</h1>
+        <div className="grid grid-cols-1 gap-3 md:gap-32 md:grid-cols-12 mt-10">
+          <div className="col-span-4">
+            {grievanceRedressalCell?.map((section, index) => (
+              <h1
+                key={index}
+                onClick={() => setSelectedIndex(index)}
+                className={`border-b-2 text-[20px] pb-3 mb-3 border-border cursor-pointer ${
+                  selectedIndex === index ? "text-[#2884CA] font-bold" : "text-textGray font-[500]"
+                }`}
+              >
+                {section.title}
+              </h1>
+            ))}
+          </div>
+          <div className="col-span-8">
+            {grievanceRedressalCell[selectedIndex]?.data && (
+              <>
+                <div>
+                  {grievanceRedressalCell[selectedIndex].data.map((item, idx) => (
+                    <p key={idx} className="text-[17px] text-textGray pb-5 lg:pe-16 mt-2">
+                      {item}
+                    </p>
+                  ))}
+                </div>
+              </>
+            )}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default PrivacySection;
