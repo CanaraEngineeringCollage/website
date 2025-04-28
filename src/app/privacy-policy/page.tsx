4eb0ebf9522302494f3d1966ff0a4092cf7c094b
CanaraEngineeringCollage <githubcec@canaraengineering.in>
2025-04-28 09:17:43 +0530

    first (#19)
    Co-authored-by: Mohamed-Hashim-km <hashimhx3@gmail.com>

---
+++ src/app/privacy-policy/page.tsx
@@ -0,0 +1,12 @@
+import PrivacySection from '@/components/PrivacyPolicyPageComponents/PrivacySection/PrivacySection'
+import React from 'react'
+
+const page = () => {
+  return (
+    <section className="px-6 md:px-12 lg:px-16 xl:px-0">
+    <PrivacySection />
+  </section>
+  )
+}
+
+export default page
