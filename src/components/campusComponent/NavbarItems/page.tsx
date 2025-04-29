3e6924887168ae1d064f0e0100f199709342a094
CanaraEngineeringCollage <githubcec@canaraengineering.in>
2025-04-29 09:32:17 +0530

    first (#22)
    Co-authored-by: Mohamed-Hashim-km <hashimhx3@gmail.com>

--- src/components/campusComponent/NavbarItems/page.tsx
+++ src/components/campusComponent/NavbarItems/page.tsx
@@ -2,11 +2,15 @@
 import About from "@/components/Common/Navbar/NavLinks/About";
 import Life from "@/components/Common/Navbar/NavLinks/Life";
 import Link from "next/link";
+import FormModal from "../FromModal/FromModal";
+import { useState } from "react";
 
 function NavbarItems() {
+  const [open, setOpen] = useState<boolean>(false);
   return (
+    <>
     <div className="hidden nav-items lg:flex text-secondary lg2 flex-row space-x-3 xl:space-x-6">
-      <div className="cursor-pointer z-100 pt-[8px] text-sm xl:text-base font-semibold transition duration-300 ease-in-out h-20 flex items-center justify-center">
+      <div className="cursor-pointer  pt-[8px] text-sm xl:text-base font-semibold transition duration-300 ease-in-out h-20 flex items-center justify-center">
         <About />
       </div>
       <div
@@ -20,20 +24,13 @@ function NavbarItems() {
         className="pt-[8px] font-poppins text-sm xl:text-base font-semibold transition duration-300 ease-in-out h-20 flex items-center justify-center"
         href={"/apply"}
       >
-        <button className="bg-none text-primary border-primary border-[1px] px-7 py-2.5 cursor-pointer rounded-[30px]">
-        Alumni Portal
-        </button>
+        <button className="bg-none text-primary border-primary border-[1px] px-7 py-2.5 cursor-pointer rounded-[30px]">Alumni Portal</button>
       </Link>
-      <Link
-        id="apply"
-        className="pt-[8px] font-poppins text-sm xl:text-base font-semibold transition duration-300 ease-in-out h-20 flex items-center justify-center"
-        href={"/apply"}
-      >
-        <button className="bg-primary text-white px-7 py-2.5 cursor-pointer rounded-[30px]">
-        Give to Canara
-        </button>
-      </Link>
+      <div onClick={()=>setOpen(true)} className="pt-[8px] font-poppins text-sm xl:text-base font-semibold transition duration-300 ease-in-out h-20 flex items-center justify-center">
+        <button className="bg-primary text-white px-7 py-2.5 cursor-pointer rounded-[30px]">Give to Canara</button>
+      </div>
     </div>
+    <FormModal isOpen={open} onClose={setOpen} /></>
   );
 }
 
