cff5c7d9a70e3312cdb03861fe0c5df1dfa93e67
sidharthmk24 <sidharthmk24@gmail.com>
2025-10-25 11:54:19 +0530

    mechanical till 2018 added

--- src/components/DepartmentComponents/DetailesPageComponents/DepartmentDetailesTab/CourseOutCome/CourseOutCome.tsx
+++ src/components/DepartmentComponents/DetailesPageComponents/DepartmentDetailesTab/CourseOutCome/CourseOutCome.tsx
@@ -36,7 +36,7 @@ const CourseOutCome = ({staticData,deptName}:{staticData:any;deptName:string }) 
 
 const admissionYears =
     deptName === "Mechanical Engineering"
-      ? ["2020-21"]: deptName === "Artificial Intelligence & Machine Learning"?generateAdmissionYears(2021,2024)
+      ? generateAdmissionYears(2018,2020): deptName === "Artificial Intelligence & Machine Learning"?generateAdmissionYears(2021,2024)
       : generateAdmissionYears();
 
 
