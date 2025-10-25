2f82dbe11c0ebbc845ca2a9c93e6c2eb28933c58
Mohamed-Hashim-km <hashimhx3@gmail.com>
2025-10-25 12:08:33 +0530

    csb co

--- src/components/DepartmentComponents/DetailesPageComponents/DepartmentDetailesTab/CourseOutCome/CourseOutCome.tsx
+++ src/components/DepartmentComponents/DetailesPageComponents/DepartmentDetailesTab/CourseOutCome/CourseOutCome.tsx
@@ -36,8 +36,8 @@ const CourseOutCome = ({staticData,deptName}:{staticData:any;deptName:string }) 
 
 const admissionYears =
     deptName === "Mechanical Engineering"
-      ? generateAdmissionYears(2018,2020): deptName === "Artificial Intelligence & Machine Learning"?generateAdmissionYears(2021,2024)
-      : generateAdmissionYears();
+      ? generateAdmissionYears(2018,2020): deptName === "Artificial Intelligence & Machine Learning"?generateAdmissionYears(2021,2024):
+     deptName === "Computer Science & Business System"?generateAdmissionYears(2021,2023) : generateAdmissionYears();
 
 
   const handleFetch = () => {
