58aff6ac896faeb55e8093cccbcd23bdc22d3c98
Mohamed-Hashim-km <hashimhx3@gmail.com>
2025-12-22 19:47:36 +0530

    updations

--- src/components/Common/Navbar/NavLinks/Life.tsx
+++ src/components/Common/Navbar/NavLinks/Life.tsx
@@ -49,7 +49,7 @@ export default function Life() {
 
     <MenuList className="w-full bg-transparent text-black p-0  z-100 border-none !border-0 shadow-none flex justify-center pb-4 outline-none focus:ring-0">
         <div className="w-full  max-w-screen-2xl bg-white z-100 shadow-md rounded-xl flex justify-center gap-8 p-8 border-none outline-none">
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 ml-28 me-5 w-full">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 px-24 w-full">
             {/* Column 1 */}
     {/* Column 1 — Campus Legacy */}
 <div className="flex gap-4 pr-4 border-r border-gray-300">
