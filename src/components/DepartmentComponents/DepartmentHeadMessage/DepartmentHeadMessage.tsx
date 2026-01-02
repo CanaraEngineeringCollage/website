69122ab309fadcb80d2fbdbddce399de7709e5cc
Mohamed-Hashim-km <hashimhx3@gmail.com>
2026-01-02 18:06:02 +0530

    updations

--- src/components/DepartmentComponents/DepartmentHeadMessage/DepartmentHeadMessage.tsx
+++ src/components/DepartmentComponents/DepartmentHeadMessage/DepartmentHeadMessage.tsx
@@ -16,10 +16,7 @@ interface DepartmentHeadMessageProps {
   return (
     <section className="w-full bg-[#051B2C]">
       <div className=" py-16  md:py-20 lg:py-20">
-          <h1 className="text-3xl lg:hidden text-center lg:text-start mb-10 md:text-[40px] leading-[1.2] lg2:text-5xl  font-bold">{depatmentHead.name === "Dr. Basappa B Kodada" 
-    ? "Message from HOD":
-    depatmentHead.name === "Dr. Raghavendra M Shetty K" ? "Message from the Department Head":
-    "Message from the Department Head"}</h1>
+          <h1 className="text-3xl lg:hidden text-center lg:text-start mb-10 md:text-[40px] leading-[1.2] lg2:text-5xl  font-bold">Message from the Head of the Department</h1>
     <div className="grid grid-cols-1 lg:w-[70%] overflow-hidden mx-auto lg:grid-cols-12  items-center">
 
           <div className=" lg:hidden block">
@@ -34,16 +31,9 @@ interface DepartmentHeadMessageProps {
         </div>
          <div className="col-span-7 lg2:px-0 lg:px-0 md:px-6 px-0">
 <h1 className="text-3xl hidden lg:block text-center lg:text-start mb-10 md:text-[40px] leading-[1.2] lg2:text-5xl font-bold">
-              {depatmentHead.name === "Dr. Basappa B Kodada" ? (
-                "Message from HOD"
-              ) : (
-                depatmentHead.name === "Dr. Raghavendra M Shetty K" ? "Message from the Department Head":
-                <>
-                  Message from the <br className="hidden xl:block" />{" "}
-                  Department Head
-                </>
-              )}
-            </h1>          <p className="text-[17px] mt-6 lg:mt-0 text-white text-justify lg:leading-[1.7]">
+          Message from the <br className="hidden lg:block" /> Head of the Department
+            </h1>         
+             <p className="text-[17px] mt-6 lg:mt-0 text-white text-justify lg:leading-[1.7]">
             {depatmentHead.messageFromtheDepartmentHead}
           </p>
         </div>
