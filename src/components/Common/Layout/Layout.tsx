4114fc9ea4cd6689e556ef7c933e3106214eaa8d
Mohamed-Hashim-km <hashimhx3@gmail.com>
2026-03-04 17:59:03 +0530

    commneted

--- src/components/Common/Layout/Layout.tsx
+++ src/components/Common/Layout/Layout.tsx
@@ -18,7 +18,7 @@ function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col min-h-screen">
       {/* overflow-x-hidden */}
-      {pathname === "/" && <CurtainScreen />}
+      {/* {pathname === "/" && <CurtainScreen />} */}
       <header className=" bg-white z-30 relative  top-0">
         {isCampusRoute ? (
           <CampusNavbar openSidebar={() => openSidebar((prev) => !prev)} sidebar={sidebar} />
